b2a8c0d4f3b6a6cedb2b7e06db33b3c4c8bf11d5
adam <adam@ubiquisoft.com>
2018-03-01 18:06:03 -0600

    Altered app.py and requirements.txt for use with heroku postgres

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import sqlite3
 from flask import Flask
 from flask_restful import Api, Resource
@@ -10,7 +11,7 @@ from resources.item import Item, Items
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'superSecretKey'
 api = Api(app)
